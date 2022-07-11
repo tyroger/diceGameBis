ce911cfcdb54ade00299043aca8ad52320f81322
roger <roger.tuyishime@gmail.com>
2022-07-11 21:38:38 +0200

    shaker class dev ongoing

--- src/game/Shaker.java
+++ src/game/Shaker.java
@@ -1,4 +1,43 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shaker {
+
+    private int shakerValue;
+    public List<Dice> dicesTable = new ArrayList<>();
+
+    // constructeur
+    public Shaker(int dicesNumber) {
+        this.setShakerValue(0);
+    }
+
+    public void setShakerValue(int shakerValue) {
+        this.shakerValue = shakerValue;
+    }
+
+    // méthodes :
+
+    /**
+     * renvoie la valeur du gobelet
+     */
+    public void getShakervalue() {
+        for (Dice dice : dicesTable
+        ) {
+            dice.
+        }
+    }
+
+    /**
+     * changer la valeur des dés dans le gobelet,met à jour sa valeur
+     */
+    public void throwDices() {
+    }
+
+
+    public void showThrowDicesValue() {
+    }
+
+
 }
