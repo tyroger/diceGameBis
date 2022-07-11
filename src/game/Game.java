004b09fe179fe458bf81a59b37b6f17b234267dd
roger <roger.tuyishime@gmail.com>
2022-07-11 23:29:08 +0200

    game class update

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,4 +1,65 @@
 package game;
 
+import java.util.List;
+
+/**
+ * créer une partie, ajouter les joueurs et définir le vainqueurs, après un nombre n de parties.
+ */
 public class Game {
+
+        private List<Player> players;
+        private int round;
+        Shaker shaker;
+
+    //_____constructeur _____ :
+        public Game(int round, int dices){
+            this.setRound(round);
+        }
+
+    //_____getters & setters _____ :
+
+    public int getRound() {
+        return round;
+    }
+
+    public void setRound(int round) {
+        this.round = round;
+    }
+
+
+    //_____méthodes _____ :
+
+    /**
+     * Ajouter un joueur à une partie
+     */
+    public void initialize(Player player){
+            players.add(player);
+    }
+
+    /**
+     * lancer une partie, et determiner le gagnant
+     */
+    public void launchGame(){
+    Player winner = players.get(0);
+        for (int i = 0; i <round; i++) {
+            for (Player player: players
+            ) {
+                player.play(shaker);
+                player.displayPlayerScore();
+
+                if(player.getScore() > winner.getScore()){
+                    winner = player;
+                }
+            }
+            this.showWinner(winner);
+        }
+    }
+
+    /**
+     * afficher qui est le vainqueur de la partie
+     */
+    public void showWinner(Player player){
+        System.out.println("le vainqueur de la partie est : " + player.getLastname());
+
+    }
 }
