08f88cc65b2ff958a1e0d33fa58def844c012e5e
roger <roger.tuyishime@gmail.com>
2022-07-11 23:44:09 +0200

    class Play modification

--- src/Play.java
+++ src/Play.java
@@ -1,5 +1,24 @@
+import game.Game;
+import game.Player;
+
 public class Play {
     public static void main(String[] args) {
+
+        int dices = 3;
+        int round = 3;
+
+        Player playerOne = new Player("toto");
+        Player playerTwo = new Player("dédé");
+
+        Game game = new Game(round, dices);
+
+        game.initialize(playerOne);
+        game.initialize(playerTwo);
+
+        game.launchGame();
+
+
+
 
     }
 }
