0816c216f5ea84a397f12ce29321558d44ea8f47
roger <roger.tuyishime@gmail.com>
2022-07-11 22:49:16 +0200

    Player class update

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,4 +1,44 @@
 package game;
 
 public class Player {
+
+    private String lastname;
+    private int score;
+
+    //_____constructeur _____ :
+
+    public Player(String lastname){
+        this.setLastname(lastname);
+        this.setScore(0);
+    }
+
+    //getters & setters _____ :
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
+    //_____Méthodes _____ :
+
+    public void play(Shaker shaker){
+        shaker.throwDices();
+        this.score += shaker.getShakervalue();
+    }
+
+    public void displayPlayerScore(){
+        System.out.println("score du joeur " + this.score);
+    }
 }
