ffc18c522ec0e0f584f6fde69b8c7db65756ac7a
roger <roger.tuyishime@gmail.com>
2022-07-11 21:16:09 +0200

    class Dice complete

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -1,4 +1,32 @@
 package game;
 
 public class Dice {
+
+    private int diceValue;
+
+    public Dice(){}
+
+    public void setDiceValue(int diceValue) {
+        this.diceValue = diceValue;
+    }
+
+    /**
+     * renvoie la valeur du dé
+     */
+    public void getDiceValue(){
+    this.setDiceValue(0);
+    }
+
+    /**
+     * change la valeur du dé.
+     * retourne une valeur aléatoire comprise entre 1 et 6
+     */
+    public int throwDice(){
+        int randomValue;
+        int minValue = 1;
+        int maxValue = 6;
+        randomValue = (int) (Math.random() * (maxValue - minValue + 1) + minValue);
+        return randomValue;
+    }
+
 }
